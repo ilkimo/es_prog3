dabc7edd4892a8bfe97abb548530b41b6c022dc7
ilkimo <vibertikim@yahoo.it>
2020-09-28 13:54:55 +0200

    fix

--- 01-intro_IntelliJ_java/src/PersonaApp.java
+++ 01-intro_IntelliJ_java/src/PersonaApp.java
@@ -5,9 +5,9 @@ public class PersonaApp {
     public static void main(String[] args) {
         Persona p1, p2, p3;
 
-        p1 = new Persona("Viberti", "Kim", 1997, 5, 24);
-        p2 = new Persona("Giovinazzo", "Valentina", 1997, 12, 06);
-        p3 = new Persona("Giovinazzo", "Melissa", 2002, 9, 27);
+        p1 = new Persona("Rossi", "Mario", 1997, 5, 24);
+        p2 = new Persona("Verdi", "Giovanna", 1997, 12, 06);
+        p3 = new Persona("Verdi", "Sabrina", 2002, 9, 27);
 
         System.out.println("oggi e': " + new GregorianCalendar(Locale.ITALIAN));
         System.out.println(p1);
