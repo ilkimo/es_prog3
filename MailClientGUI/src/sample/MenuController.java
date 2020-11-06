93f94b1ed596f3590f2360ba3bab47e3bca06c85
Livio <livio@iMac-di-Livio.local>
2020-11-06 10:42:45 +0100

    EDITED: now handling on NewMail button opens a new GUI

--- MailClientGUI/src/sample/MenuController.java
+++ MailClientGUI/src/sample/MenuController.java
@@ -2,9 +2,15 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 import java.io.File;
+import java.io.IOException;
 
 public class MenuController {
     private EmailDataModel model ;
@@ -22,38 +28,49 @@ public class MenuController {
     }
     
     @FXML
-    private void handleNewMailButton(ActionEvent event) {
-        //Handle Click Here
+    private void handleNewMailButton(ActionEvent event) throws IOException {
+        //TODO: Handle Click Here
         System.out.println("New Mail Button Clicked");
+    
+        BorderPane root = new BorderPane();
+        
+        //TODO: load new mail GUI
+        FXMLLoader listLoader = new FXMLLoader(getClass().getResource("list.fxml"));
+        root.setLeft(listLoader.load());  //mette al sinistra la vista
+        
+        Stage newMailStage = new Stage();
+        Scene scene = new Scene(root, 900, 550);
+        newMailStage.setScene(scene);
+        newMailStage.show();
     }
     
     @FXML
     private void handleRefreshMailboxButton(ActionEvent event) {
-        //Handle Click Here
+        //TODO: Handle Click Here
         System.out.println("Refresh MailBox Button Clicked");
     }
     
     @FXML
     private void handleReplyButton(ActionEvent event) {
-        //Handle Click Here
+        //TODO: Handle Click Here
         System.out.println("Reply Button Clicked");
     }
     
     @FXML
     private void handleReplyAllButton(ActionEvent event) {
-        //Handle Click Here
+        //TODO: Handle Click Here
         System.out.println("Reply All Button Clicked");
     }
     
     @FXML
     private void handleForwardButton(ActionEvent event) {
-        //Handle Click Here
+        //TODO: Handle Click Here
         System.out.println("Forward Button Clicked");
     }
     
     @FXML
     private void handleDeleteButton(ActionEvent event) {
-        //Handle Click Here
+        //TODO: Handle Click Here
         System.out.println("Delete Button Clicked");
     }
     
